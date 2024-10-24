142a2ccdd844d74e2f391819c0f4bc53b795cc61
ArtManhas <arthurhcqh@gmail.com>
2024-10-24 09:44:20 -0300

    Integração do Back com o Front

--- Backend-TIAF-Lista-Tarefas-main/app.js
+++ Backend-TIAF-Lista-Tarefas-main/app.js
@@ -1,20 +1,16 @@
 const express = require('express');
-const cors = require('cors'); // Importa o CORS
+const cors = require('cors'); 
 const app = express();
 const tarefaRoutes = require('./routes/tarefaRoutes');
 const userRoutes = require('./routes/userRoutes');
 const authMiddleware = require('./middlewares/authMiddleware');
 
-// Middleware para JSON
 app.use(express.json());
 
-// Inicializa o CORS
-app.use(cors()); // Aqui é onde você deve inicializar o CORS
+app.use(cors()); 
 
-// Rotas
-app.use('/api', userRoutes); // Rota para autenticação
+app.use('/api', userRoutes); 
 
-// Rotas protegidas
 app.use('/api/tarefas', authMiddleware, tarefaRoutes);
 
 module.exports = app;
